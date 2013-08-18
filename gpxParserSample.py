080bf3990bcaf3441cc7f29047256e04d47c5453
Matthias Stuebner <mstuebner@gmail.com>
2013-08-18 22:08:35 +0200

    changed to reflect actual code

--- gpxParserSample.py
+++ gpxParserSample.py
@@ -2,27 +2,31 @@ import gpx_parser.gpx_parser as gpx_parser
 
 if __name__ == '__main__':
     
-    gpx = gpx_parser.gpx_parser('Track_002.gpx')
+    gpx = gpx_parser.gpx_file('Track_002.gpx')
     
     tracks = gpx.getTracks()
     
-    for trackname in tracks:
-        print('Name: ' + trackname)
+    for track in tracks:
+        print('Name: ' + track.getTrackname())
         
-        gpx.reduceTrackpointsByDistance(name=trackname)
+        track.reduceTrackpointsByDistance()
         
-        trackpoints = gpx.getTrackpoints(name=trackname)
-        sum_distance = gpx.getTrackDistance(name=trackname)
+        trackpoints = track.getTrackpoints()
+        sum_distance = track.getTrackDistance()
+        min_ele, max_ele = track.getMinMaxElevation()
 
         for trackpoint in trackpoints:
-
-            dist = gpx.getDistanceToPrevPoint(name=trackname, trkpoint=trackpoint)
-
+ 
+            dist = track.getDistanceToPrevPoint(trkpoint=trackpoint)
+ 
             print('    Track point: {}, Lat: {}, Lon: {}, Distance to previous point: {}'.format(trackpoint.time,
                                                                                                  trackpoint.lat,
                                                                                                  trackpoint.lon,
                                                                                                  dist)
                   )
             
-        print('\nNumber of track points: {}\nDistance: {}'.format(gpx.getNumTrackpoints(trackname),
-                                                                sum_distance))
+        print('\nNumber of track points: {}\nDistance: {}\nmin evelation: {}\nmax. elevation: {}'.format(track.getNumTrackpoints(),
+                                                                                                         sum_distance,
+                                                                                                         min_ele,
+                                                                                                         max_ele)
+              )
