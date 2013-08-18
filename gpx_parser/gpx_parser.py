f25b3fbca6f70cba596b8cb3c9955ae9bbb804e3
Matthias Stuebner <mstuebner@gmail.com>
2013-08-18 22:05:11 +0200

    split into gpx_file and gpx_track

--- gpx_parser/gpx_parser.py
+++ gpx_parser/gpx_parser.py
@@ -13,10 +13,10 @@ gpx_track = namedtuple('track',
                        )
 
 
-class gpx_parser:
+class gpx_file:
     
     def __init__(self, filename):
-        self.tracks = {}
+        self.tracks = []
         try:
             doc = minidom.parse(filename)
             doc.normalize()
@@ -25,14 +25,12 @@ class gpx_parser:
         
         gpx = doc.documentElement
         for node in gpx.getElementsByTagName('trk'):
-            self.parseTrack(node)
+            self.tracks.append(self.parseTrack(node))
     
     def parseTrack(self, track):
-        
-        name = track.getElementsByTagName('name')[0].firstChild.data
-        
-        if not name in self.tracks:
-            self.tracks[name] = []
+
+        _trackname = track.getElementsByTagName('name')[0].firstChild.data
+        _trackpoints = []
             
         for track_seg in track.getElementsByTagName('trkseg'):
             for track_point in track_seg.getElementsByTagName('trkpt'):
@@ -45,24 +43,34 @@ class gpx_parser:
                 
                 try:
                     rfc3339 = track_point.getElementsByTagName('time')[0].firstChild.data
-                    #self.tracks[name][rfc3339]={'lat':lat, 'lon':lon, 'ele':ele}
                 except:
-                    rfc3339 = len(self.tracks[name])
-                    #self.tracks[name][len(self.tracks[name])]={'lat':lat, 'lon':lon, 'ele':ele}
+                    rfc3339 = len(_trackpoints)
                     
-                self.tracks[name].append(gpx_trackpoint(rfc3339, lon, lat, ele))
-                        
-    def getTrackpoints(self, name):
-        trackpoints = self.tracks[name]
-        return trackpoints
+                _trackpoints.append(gpx_trackpoint(rfc3339, lon, lat, ele))
 
-    def getTrackDistance(self, name):
-        trackpoints = self.getTrackpoints(name)
+        return gpx_track(_trackname, _trackpoints)
+
+    def getTracks(self):
+        return self.tracks
+
+
+class gpx_track:
+    
+    def __init__(self, trackname, trackpoints):
+        self.trackname = trackname
+        self.trackpoints = trackpoints
+        self.sum_distance = self.getTrackDistance()
+        self.num_trackpoints = self.getNumTrackpoints() 
+        
+    def getTrackname(self):
+        return self.trackname
+
+    def getTrackDistance(self):
         old_lat = 0 
         old_lon = 0
         sum_distance = 0
     
-        for trackpoint in trackpoints:
+        for trackpoint in self.trackpoints:
                 
             if old_lat > 0 and old_lon > 0:
                 dist = gpx_data.gpx_data().get_distance_from_lat_lon(lat1=old_lat, 
@@ -79,47 +87,52 @@ class gpx_parser:
 
         return sum_distance
     
-    def getDistanceToPrevPoint(self, name, trkpoint):
-        trackpoints = self.getTrackpoints(name)
-        idx = trackpoints.index(trkpoint)
+    def getTrackpoints(self):
+        return self.trackpoints
+
+    def getDistanceToPrevPoint(self, trkpoint):
+        idx = self.trackpoints.index(trkpoint)
         
         if idx > 0:
-            dist = gpx_data.gpx_data().get_distance_from_lat_lon(lat1=trackpoints[idx-1].lat, 
-                                                                 lon1=trackpoints[idx-1].lon, 
+            dist = gpx_data.gpx_data().get_distance_from_lat_lon(lat1=self.trackpoints[idx-1].lat, 
+                                                                 lon1=self.trackpoints[idx-1].lon, 
                                                                  lat2=trkpoint.lat, 
                                                                  lon2=trkpoint.lon)
             return dist
 
-    def getTracks(self):
-        return self.tracks
+    def getNumTrackpoints(self):
+        return len(self.trackpoints)
     
-    def getNumTrackpoints(self, name):
-        return len(self.getTrackpoints(name))
+    def removeTrackpoint(self, trkpoint):
+        del self.trackpoints[self.trackpoints.index(trkpoint)]
     
-    def removeTrackpoint(self, name, trkpoint):
-        trackpoints = self.getTrackpoints(name)
-        del trackpoints[trackpoints.index(trkpoint)]
-    
-    def reduceTrackpointsByDistance(self, name, min_distance=0.5):
-        trackpoints = self.getTrackpoints(name)
-        
-        for trackpoint in trackpoints:
-            if trackpoints.index(trackpoint) > 0:
-                dist = self.getDistanceToPrevPoint(name, trackpoint)
+    def reduceTrackpointsByDistance(self, min_distance=0.1):
+        for trackpoint in self.trackpoints:
+            if self.trackpoints.index(trackpoint) > 0:
+                dist = self.getDistanceToPrevPoint(trackpoint)
                 if dist < min_distance:
-                    self.removeTrackpoint(name, trackpoint)
+                    self.removeTrackpoint(trackpoint)
+
+    def getMinMaxElevation(self):
+        elevation = []
+        
+        for trackpoint in self.trackpoints:
+            elevation.append(trackpoint.ele)
+            
+        return min(elevation), max(elevation)
+
 
 if __name__ == '__main__':
     
-    gpx = gpx_parser('d:\\temp\\Track_002.gpx')
+    gpx = gpx_file('d:\\temp\\Track_002.gpx')
     
     tracks = gpx.getTracks()
     
     for track in tracks:
-        print('Name: ' + track)
+        print('Name: ' + track.getTrackname())
         
-        trackpoints = gpx.getTrackpoints(name=track)
-        distance = gpx.getTrackDistance(name=track)
+        trackpoints = track.getTrackpoints()
+        distance = track.getTrackDistance()
 
         print('Distance: {}'.format(distance))
     
