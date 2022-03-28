90df8fe5707f06d347063bf917643753a272da5d
mstuebner <mstuebner@gmail.com>
2022-03-28 10:45:46 +0200

    Re-formatting

--- examples/gpxParserSample.py
+++ examples/gpxParserSample.py
@@ -9,25 +9,19 @@ import py_gpx.gpx_parser.gpx_parser as gpx_parser
 if __name__ == '__main__':
      
     gpx = gpx_parser.gpx_file('Track_002.gpx')
-     
     tracks = gpx.getTracks()
      
     for track in tracks:
         print('Name: ' + track.getTrackname())
-         
         track.reduceTrackpointsByDistance()
-         
         trackpoints = track.getTrackpoints()
  
         for trackpoint in trackpoints:
-  
-            print('    Track point: {}, Lat: {}, Lon: {}, Distance to previous point: {}'.format(trackpoint.time,
+              print('    Track point: {}, Lat: {}, Lon: {}, Distance to previous point: {}'.format(trackpoint.time,
                                                                                                  trackpoint.lat,
                                                                                                  trackpoint.lon,
-                                                                                                 track.getDistanceToPrevPoint(trkpoint=trackpoint))
-                  )
+                                                                                                 track.getDistanceToPrevPoint(trkpoint=trackpoint)))
          
         print('\nNumber of track points: {}\nDistance: {}\nmin evelation: {}\nmax. elevation: {}'.format(track.getNumTrackpoints(),
                                                                                                          track.getTrackDistance(),
-                                                                                                         *track.getMinMaxElevation())
-              )
+                                                                                                         *track.getMinMaxElevation()))
